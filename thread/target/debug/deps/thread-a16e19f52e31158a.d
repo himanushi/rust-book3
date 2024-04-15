db2ca6321c41d9938891161b5b3c3cb3e0a88994
himanushi <himanushi@users.noreply.github.com>
2024-04-15 16:37:43 +0900

    fix

--- thread/target/debug/deps/thread-a16e19f52e31158a.d
+++
@@ -1,5 +0,0 @@
-/Users/tokumei/Work/rust-book3/thread/target/debug/deps/libthread-a16e19f52e31158a.rmeta: src/main.rs
-
-/Users/tokumei/Work/rust-book3/thread/target/debug/deps/thread-a16e19f52e31158a.d: src/main.rs
-
-src/main.rs:
