89c53c8529ca58e0d3bc091b6ebe983ccd2703cf
himanushi <himanushi@users.noreply.github.com>
2024-04-15 17:30:35 +0900

    p.5

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -2,13 +2,17 @@ use std::thread;
 
 fn main() {
     let numbers = vec![1, 2, 3];
-    thread::spawn(move || {
-        for n in numbers {
-            println!("{n}");
-        }
-    })
-    .join()
-    .unwrap();
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            println!("length: {}", numbers.len());
+        });
+        s.spawn(|| {
+            for n in &numbers {
+                println!("{n}");
+            }
+        });
+    });
 
     println!("Main!!");
 }
