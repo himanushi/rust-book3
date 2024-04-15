bad3b1dbbbaecf7ac3a828c71de797d3e3c5e29c
himanushi <himanushi@users.noreply.github.com>
2024-04-15 16:30:24 +0900

    cargo new thread

---
+++ thread/target/debug/deps/thread-a16e19f52e31158a.d
@@ -0,0 +1,5 @@
+/Users/tokumei/Work/rust-book3/thread/target/debug/deps/libthread-a16e19f52e31158a.rmeta: src/main.rs
+
+/Users/tokumei/Work/rust-book3/thread/target/debug/deps/thread-a16e19f52e31158a.d: src/main.rs
+
+src/main.rs:
---
+++ thread/target/debug/deps/thread-bf0de073297198d9.d
@@ -0,0 +1,5 @@
+/Users/tokumei/Work/rust-book3/thread/target/debug/deps/libthread-bf0de073297198d9.rmeta: src/main.rs
+
+/Users/tokumei/Work/rust-book3/thread/target/debug/deps/thread-bf0de073297198d9.d: src/main.rs
+
+src/main.rs:
